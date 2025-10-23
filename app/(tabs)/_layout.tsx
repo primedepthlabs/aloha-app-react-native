8237f1d869345c8aad934705f9c0a8ed671bd345
gagandeep kaur <gagandeepkaur@Mac.lan>
2025-10-23 10:29:28 +0530

    messages,call,video and chat completed

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -95,6 +95,12 @@ export default function TabLayout() {
           href: null,
         }}
       />
+      <Tabs.Screen
+        name="chats/chat"
+        options={{
+          href: null,
+        }}
+      />
     </Tabs>
   );
 }
