5ad7f5c0ac5b19e48bf1901673f6c91255a45849
Priyanshu <advpriyanshu6@gmail.com>
2025-11-13 01:59:37 +0530

    prebuild done

--- app/(tabs)/test.tsx
+++ app/(tabs)/test.tsx
@@ -1,8 +1,10 @@
+import { useRouter } from 'expo-router'
 import React from 'react'
 import { Text, View } from 'react-native'
 
 export default function test() {
+    const router = useRouter()
     return (
-        <View className='bg-red-500 flex-1'><Text>test</Text></View>
+        <View className='bg-red-500 flex-1 justify-center items-center'><Text>test</Text></View>
     )
 }
